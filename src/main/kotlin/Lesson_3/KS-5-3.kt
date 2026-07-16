7b2a71dafeca008a0fcb048bda36e98159d5c117
AlexGogolin <agogolin89@mail.ru>
2026-07-16 12:46:43 +0300

    KS-5-3 done

---
+++ src/main/kotlin/Lesson_3/KS-5-3.kt
@@ -0,0 +1,25 @@
+package org.example.Lesson_3
+
+fun main() {
+
+    val num1 = 15
+    val num2 = 34
+    println("Введите число от 0 до 42")
+
+    println("Введите первое число")
+    val userNum1 = readln().toIntOrNull()
+
+    println("Введите второе число")
+    val userNum2 = readln().toIntOrNull()
+
+    val firstMatch = num1 == userNum1 || num1 == userNum2
+    val secondMatch = num2 == userNum1 || num2 == userNum2
+
+    val matchCount = (if (firstMatch) 1 else 0) + (if (secondMatch) 1 else 0)
+
+    if (matchCount == 2) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    }else if (matchCount == 1){
+        println("Вы выиграли утешительный приз!")
+    }else println("Неудача!")
+}
