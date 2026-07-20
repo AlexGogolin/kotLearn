2bb28f5e8a866b2547d7fb6e121cb6d54ba90a48
AlexGogolin <agogolin89@mail.ru>
2026-07-20 10:24:04 +0300

    KS-6-4 fix

--- src/main/kotlin/Lesson_4/KS-6-4.kt
+++ src/main/kotlin/Lesson_4/KS-6-4.kt
@@ -1,21 +1,23 @@
 package org.example.Lesson_4
 
+const val QUANTITY_OF_ATTEMPTS = 5
+
 fun main() {
-    val secretNumber = 7
+    val secretNumber = (1..9).random()
     var turn = 1
 
     println("Попробуй угадать секретное число от 1 до 9")
-    while (turn <= 5) {
+    while (turn <= QUANTITY_OF_ATTEMPTS) {
         println("Попытка № $turn")
         val answer = readln().toInt()
         if (answer == secretNumber) {
             println("Это была великолепная игра!")
             break
         }
-        println("Неверно, осталось попыток ${5 - turn}")
+        println("Неверно, осталось попыток ${QUANTITY_OF_ATTEMPTS - turn}")
         turn++
     }
-    if (turn > 5) {
+    if (turn > QUANTITY_OF_ATTEMPTS) {
         println("Было загадано число $secretNumber")
     }
 }
