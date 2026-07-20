719e942bf13f699a4121c74bc0f083796135d310
AlexGogolin <agogolin89@mail.ru>
2026-07-20 13:30:35 +0300

    KS-7-2 fix

--- src/main/kotlin/lesson_5/KS-7-2.kt
+++ src/main/kotlin/lesson_5/KS-7-2.kt
@@ -4,19 +4,16 @@ import kotlin.random.Random
 
 fun main() {
 
-    var password = ""
-    val numbers = "0123456789"
-    for (i in 1..4) {
-        password += numbers[(Random.nextInt(numbers.length))]
+    var password: Int = Random.nextInt(1000, 10000)
 
-    }
     println("Ваш код авторизации: $password")
     println("Введите код авторизации")
-    var userAnswer = readln()
+    var userAnswer = readln().toInt()
     while (userAnswer != password) {
+        password = Random.nextInt(1000, 10000)
         println("Неверно, ваш код авторизации $password")
         println("Попробуйте еще раз")
-        userAnswer = readln()
+        userAnswer = readln().toInt()
     }
 
     println("Вы авторизовались!!")
