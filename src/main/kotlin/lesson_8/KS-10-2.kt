fdcb580f436b5d80ffe05e1c6176663c54d13e22
AlexGogolin <agogolin89@mail.ru>
2026-07-22 12:34:04 +0300

    KS-10-2 fix

--- src/main/kotlin/lesson_8/KS-10-2.kt
+++ src/main/kotlin/lesson_8/KS-10-2.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_8
 
+fun validateInput(login: String, password: String): Boolean {
+    val isValidate = login.length >= 4 && password.length >= 4
+    return isValidate
+}
+
 fun main() {
     System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
     println("Введите логин")
     val login = readln()
     println("Введите пароль")
     val password = readln()
-
-    fun validateInput(login: String, password: String): Boolean {
-        val isValidate = login.length > 4 && password.length > 4
-        return isValidate
-    }
 
     if (!validateInput(login = login, password = password)) {
         println("Логин или пароль недостаточно длинные")
