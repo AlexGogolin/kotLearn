36120cce18c327b8c19ee433216df29c4d92b085
AlexGogolin <agogolin89@mail.ru>
2026-07-22 13:05:53 +0300

    KS-10-3 fix

--- src/main/kotlin/lesson_8/KS-10-3.kt
+++ src/main/kotlin/lesson_8/KS-10-3.kt
@@ -6,15 +6,18 @@ fun main() {
     System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
     val symbols: String = "!\"#\$%&'()*+,-./ "
     val numbers: String = "0123456789"
-    var password = ""
 
     println("Какой длины пароль вы бы хотели?")
     val passwordLength = readln().toInt()
 
-    fun passwordGenerator(length: Int): String{
-        for (i in 1 .. length / 2){
-            password += symbols[Random.nextInt(symbols.length)]
-            password += numbers[Random.nextInt(numbers.length)]
+    fun passwordGenerator(length: Int): String {
+        var password: String = ""
+        for (i in 1..length) {
+            if (i % 2 == 0) {
+                password += symbols[Random.nextInt(symbols.length)]
+            } else {
+                password += numbers[Random.nextInt(numbers.length)]
+            }
         }
         return password
     }
