07879873e4b617a2de56abf9661dcc469aab53e4
AlexGogolin <agogolin89@mail.ru>
2026-07-23 10:48:14 +0300

    KS-11-5 fix

--- src/main/kotlin/lesson_9/KS-11-5.kt
+++ src/main/kotlin/lesson_9/KS-11-5.kt
@@ -6,7 +6,8 @@ class Forum {
     private val messages: MutableList<ForumMessage> = mutableListOf()
 
     fun createNewUser(userName: String): ForumUser {
-        val newUser = ForumUser(nextUserId, userName)
+        val builder = ForumUser.Builder()
+        val newUser = builder.build(nextUserId, userName)
         nextUserId++
         users.add(newUser)
         return newUser
@@ -15,7 +16,8 @@ class Forum {
     fun createNewMessage(authorId: Int, message: String) {
         val oldUser = users.find { it.userId == authorId }
         if (oldUser != null) {
-            val newMessage = ForumMessage(authorId, message)
+            val builder = ForumMessage.Builder()
+            val newMessage = builder.build(authorId, message)
             messages.add(newMessage)
         }
     }
@@ -23,20 +25,32 @@ class Forum {
     fun printThread() {
         for (message in messages) {
             val author = users.find { it.userId == message.authorId }
-            println("${author!!.userName}: ${message.message}")
+            println("${author?.userName ?: "Неизвестный"}: ${message.message}")
         }
     }
 }
 
-class ForumUser(
+class ForumUser private constructor(
     val userId: Int,
     val userName: String,
-)
+) {
+    class Builder {
+        fun build(userId: Int, userName: String): ForumUser {
+            return ForumUser(userId, userName)
+        }
+    }
+}
 
-class ForumMessage(
+class ForumMessage private constructor(
     val authorId: Int,
     val message: String,
-)
+) {
+    class Builder {
+        fun build(authorId: Int, message: String): ForumMessage {
+            return ForumMessage(authorId, message)
+        }
+    }
+}
 
 fun main() {
     val forum = Forum()
@@ -44,6 +58,7 @@ fun main() {
     forum.createNewUser("Nina")
     forum.createNewMessage(1, "Hallo")
     forum.createNewMessage(2, "Hi")
+    forum.createNewMessage(1, "How are you?")
+    forum.createNewMessage(2, "Fine, thx")
     forum.printThread()
-
 }
