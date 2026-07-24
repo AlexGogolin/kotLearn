eb3ee0dec4b58abba24d97ac1c8b1ad68eb81e77
AlexGogolin <agogolin89@mail.ru>
2026-07-24 12:19:56 +0300

    KS-13-5 fix

--- src/main/kotlin/lesson_11/KS-13-5.kt
+++ src/main/kotlin/lesson_11/KS-13-5.kt
@@ -11,32 +11,17 @@ class Subscriber5(
 }
 
 fun main() {
-    val subscribers = mutableListOf<Subscriber5>()
-    while (true) {
-        println("Введите данные абонента")
-        println("Введите Имя:")
-        val name = readln()
-        println("Введите номер телефона: и компанию")
-        val phoneNumber: String = readln()
-        println("Введите название компании:")
-        val company: String? = readln().ifEmpty { null }
-        var phoneNumberLong: Long?
-        try {
-            phoneNumberLong = phoneNumber.toLong()
-        } catch (e: NumberFormatException) {
-            println(e)
-            phoneNumberLong = null
-        }
-        if (phoneNumberLong != null) {
-            subscribers.add(Subscriber5(name, phoneNumberLong, company))
-        }
-        println("Хотите добавить еще контакт?")
-        val answer = readln()
-        if (!answer.equals("Да", ignoreCase = true)) {
-            break
-        }
+
+    println("Введите данные абонента")
+    println("Введите номер телефона:")
+    val phoneNumber: String = readln()
+    var phoneNumberLong: Long?
+    try {
+        phoneNumberLong = phoneNumber.toLong()
+    } catch (e: NumberFormatException) {
+        println(e::class.simpleName)
+        phoneNumberLong = null
     }
-    subscribers.forEach {
-        it.printSubscriber()
-    }
+
+
 }
