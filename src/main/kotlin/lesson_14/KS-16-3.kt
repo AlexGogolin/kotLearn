9c598ffc1523e2dfe7c58d33733447fd48c9b470
AlexGogolin <agogolin89@mail.ru>
2026-07-28 14:50:21 +0300

    KS-16-3 fix 1.0

--- src/main/kotlin/lesson_14/KS-16-3.kt
+++ src/main/kotlin/lesson_14/KS-16-3.kt
@@ -2,8 +2,8 @@ package org.example.lesson_14
 
 class User(
     private val login: String,
+    private val password: String = "qwerty",
 ) {
-    private val password: String = "qwerty"
     fun isValidate(userPassword: String): Boolean {
         return userPassword == password
     }
@@ -11,5 +11,5 @@ class User(
 
 fun main() {
     val user = User(login = "admin")
-    println(user.isValidate("qwerty"))
+    println(user.isValidate(readln()))
 }
