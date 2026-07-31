8cf830b70bb68044b28bd108cf298136c1873608
AlexGogolin <agogolin89@mail.ru>
2026-07-31 13:46:26 +0300

    KS-17-2 fix

--- src/main/kotlin/lesson_15/KS-17-2.kt
+++ src/main/kotlin/lesson_15/KS-17-2.kt
@@ -1,13 +1,9 @@
 package org.example.lesson_15
 
 class Ship(name: String, val speed: Int, val base: String) {
-
-
     var name: String = name
         set(value) {
-            if (value != field) {
-                println("Имя не может быть изменено")
-            }
+            println("Имя не может быть изменено")
         }
 }
 
