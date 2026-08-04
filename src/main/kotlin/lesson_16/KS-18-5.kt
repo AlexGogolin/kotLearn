a616141907a5914c9e28878fdcf177f658495c80
AlexGogolin <agogolin89@mail.ru>
2026-08-04 14:43:18 +0300

    KS-18-5 fix

--- src/main/kotlin/lesson_16/KS-18-5.kt
+++ src/main/kotlin/lesson_16/KS-18-5.kt
@@ -1,56 +1,52 @@
 package org.example.lesson_16
 
-open class Screen(val type: String) {
+open class Screen{
     fun drawCircle(x: Int, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Круг -> с координатами $x $y"
     }
 
     fun drawCircle(x: Int, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Круг -> с координатами $x $y"
     }
 
     fun drawCircle(x: Float, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Круг -> с координатами $x $y"
     }
 
     fun drawCircle(x: Float, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Круг -> с координатами $x $y"
     }
 
     fun drawSquare(x: Int, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Квадрат -> с координатами $x $y"
     }
 
     fun drawSquare(x: Int, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Квадратe -> с координатами $x $y"
     }
 
     fun drawSquare(x: Float, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Квадрат -> с координатами $x $y"
     }
 
     fun drawSquare(x: Float, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Квадрат -> с координатами $x $y"
     }
 
     fun drawPoint(x: Int, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Точка -> с координатами $x $y"
     }
 
     fun drawPoint(x: Int, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Точка -> с координатами $x $y"
     }
 
     fun drawPoint(x: Float, y: Int): String {
-        return "$type -> с координатами $x $y"
+        return "Точка -> с координатами $x $y"
     }
 
     fun drawPoint(x: Float, y: Float): String {
-        return "$type -> с координатами $x $y"
+        return "Точка -> с координатами $x $y"
     }
 }
-
-class Circle() : Screen("Circle")
-class Square() : Screen("Square")
-class Point() : Screen("Point")
 
