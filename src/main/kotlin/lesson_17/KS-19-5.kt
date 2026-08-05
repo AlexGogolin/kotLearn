370e117af5b9cb81b8083bc4aa4b2e279c4f909a
AlexGogolin <agogolin89@mail.ru>
2026-08-05 16:15:29 +0300

    KS-19-5 done

---
+++ src/main/kotlin/lesson_17/KS-19-5.kt
@@ -0,0 +1,37 @@
+package org.example.lesson_17
+
+enum class Gender(val translateGender: String) {
+    MALE("Муж"),
+    FEMALE("Жен")
+}
+
+class Person(
+    val name: String,
+    val gender: Gender,
+)
+
+fun main() {
+    val listOfPersons = mutableListOf<Person>()
+    var counter = 1
+    println("В картотеку нужно внести 5 человек.")
+
+    while (listOfPersons.size < 2) {
+        println("№$counter Пожалуйста введите имя:")
+        val name = readln()
+        println("теперь пол: \"Муж\" или \"Жен\"")
+        val genderInput = readln()
+
+        val gender = when (genderInput) {
+            "Муж" -> Gender.MALE
+            "Жен" -> Gender.FEMALE
+            else -> {
+                println("Ошибка! Неверный пол. Попробуем еще раз для этого человека.")
+                continue
+            }
+        }
+        listOfPersons.add(Person(name, gender))
+        counter++
+    }
+    println("Итоговый список:")
+    listOfPersons.forEach { println("${it.name} ${it.gender.translateGender}") }
+}
